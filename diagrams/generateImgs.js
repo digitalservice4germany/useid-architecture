5dbacde7c94ef84fa5bcbfce6f1e46916f148e3d
mpanne <merlin.pannewitz@digitalservice.bund.de>
2022-10-19 13:36:01 +0200

    Refactor and cleanup repository structure
    - Add diagrams directory
    - Add image generation script and workflow

---
+++ diagrams/generateImgs.js
@@ -0,0 +1,86 @@
+const { execSync } = require("child_process");
+const axios = require("axios");
+const { chromium } = require("playwright");
+const workspacePage = "http://localhost:8080/workspace/diagrams/";
+
+function run(cmd) {
+  execSync(cmd, function (err, stdout, stderr) {
+    console.log(stdout);
+    console.error(stderr);
+    if (err) {
+      console.error(err);
+    }
+  });
+}
+
+async function startContainer() {
+  run(
+    'docker pull structurizr/lite && docker run --name structurizr -d --rm -p 8080:8080 -v "$(pwd):/usr/local/structurizr" structurizr/lite'
+  );
+
+  // wait for container to become available
+  const sleepInterval = 500; //ms
+  let start = Date.now();
+  while (true) {
+    if (Date.now() - start > 60000) {
+      throw new Error("container did not become available after 60s");
+    }
+
+    try {
+      await axios(workspacePage);
+      break;
+    } catch {
+      await new Promise((resolve) => setTimeout(resolve, sleepInterval));
+    }
+  }
+}
+
+function stopContainer() {
+  run("docker kill structurizr");
+}
+
+async function downloadImgs() {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(workspacePage);
+
+  // accept terms - only on first run, so we allow this to fail
+  try {
+    await page.click('text=Accept', { timeout: 5000 });
+  } catch {
+    // ok, probably already accepted
+  }
+
+  // allow workspace to load fully
+  await page.waitForLoadState("networkidle");
+
+  await page.click('text=× Diagram editor >> [aria-label="Close"]');
+  await page.click('[title="Export diagram and key/legend to PNG"]');
+  await page.check("text=Crop diagrams");
+  await page.check("text=Automatically download");
+
+  page.on("download", async (download) => {
+    await download.saveAs("./imgs/" + download.suggestedFilename());
+    await download.delete();
+  });
+
+  await page.click("text=Export all diagrams");
+
+  // allow all downloads to complete
+  await page.waitForLoadState("networkidle");
+
+  // wait for the download and delete the temporary file
+  await browser.close();
+}
+
+(async () => {
+  try {
+    await startContainer();
+    await downloadImgs();
+  } catch(error) {
+    console.error(error)
+    process.exit(1)
+  } finally {
+    stopContainer();
+  }
+})();
